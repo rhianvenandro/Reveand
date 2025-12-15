87a11d340ec656fcc4a27579efc7e4c252979c27
Rhian Venandro <249495677+rhianvenandro@users.noreply.github.com>
2025-12-14 21:04:03 -0300

    Delete RablueWay directory

--- RablueWay/header.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-  <meta charset="UTF-8">
-  <title><?php bloginfo('RableWay'); ?></title>
-  <?php wp_head(); ?>
-</head>
-<body>
--- RablueWay/index.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-
-<main>
-  <h1>RableWay</h1>
-</main>
-
-<?php get_footer(); ?>
